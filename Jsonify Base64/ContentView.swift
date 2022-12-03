e1aada4d023f42c6719c1a52a1e7e0afb320d148
Daniel Lotz <daniel.lotz@servicenow.com>
2022-12-03 09:44:19 +0200

    Initial Commit

---
+++ Jsonify Base64/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Jsonify Base64
+//
+//  Created by Daniel Lotz on 03/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Jsonify Base64/Jsonify_Base64App.swift
@@ -0,0 +1,17 @@
+//
+//  Jsonify_Base64App.swift
+//  Jsonify Base64
+//
+//  Created by Daniel Lotz on 03/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Jsonify_Base64App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
